6a26af3c75b5bf6ede64cf0cb9a7ab0016a455c4
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 20:54:03 +0100

    feat: create localization services barrel export

--- src/modules/general/localization/services/index.ts
+++ src/modules/general/localization/services/index.ts
@@ -1,8 +1,13 @@
 /**
- * Administration Services - Central Export
+ * Localization Services
+ * Exports all services related to localization entities
+ * 
+ * Aligned with backend: dz.sh.trc.hyflo.general.localization.service
+ * 
+ * @author CHOUABBIA Amine
+ * @created 01-03-2026
  */
 
-
-export { countryService } from './countryService';
-export { stateService } from './stateService';
-export { localityService } from './localityService';
+export { countryService } from './CountryService';
+export { stateService } from './StateService';
+export { localityService } from './LocalityService';
