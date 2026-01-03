ef548a578f5666a1b26a13b4c6e9cd0639a1902e
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 20:59:21 +0100

    refactor: update common/administration services to re-export from general/localization for backward compatibility

--- src/modules/common/administration/services/index.ts
+++ src/modules/common/administration/services/index.ts
@@ -1,10 +1,17 @@
 /**
- * Administration Services
- * Exports all services related to administration entities
+ * Administration Services - Legacy Compatibility Layer
+ * Re-exports services from new localization module for backward compatibility
+ * 
+ * DEPRECATED: Use imports directly from src/modules/general/localization/services
+ * This file is kept for backward compatibility with existing components
+ * 
+ * New Import Path:
+ * import { countryService, stateService, localityService } from '../../../general/localization/services';
  * 
  * @author CHOUABBIA Amine
  * @created 01-03-2026
+ * @updated 01-03-2026 (refactored for architecture alignment)
  */
 
-export { stateService } from './StateService';
-export { localityService } from './LocalityService';
+// Re-export all localization services from their new proper location
+export { countryService, stateService, localityService } from '../../../general/localization/services';
