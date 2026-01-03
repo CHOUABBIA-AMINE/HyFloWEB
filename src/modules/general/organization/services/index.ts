87767b1cd1cf3ec32eaf919ec522225d9630ffbb
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 21:19:56 +0100

    fix: add missing organization service exports (employeeService, structureService, jobService)

--- src/modules/general/organization/services/index.ts
+++ src/modules/general/organization/services/index.ts
@@ -6,7 +6,15 @@
  * 
  * @author CHOUABBIA Amine
  * @created 01-03-2026
+ * @updated 01-03-2026 - Added organization service exports
  */
+
+// Organization services (default exports)
+export { default as structureService } from './StructureService';
+export { default as jobService } from './JobService';
+
+// Organization services (named exports)
+export { employeeService } from './employeeService';
 
 // Re-export localization services that organization depends on
 export { countryService, stateService, localityService } from '../../localization/services';
