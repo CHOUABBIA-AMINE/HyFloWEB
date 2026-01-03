67f36579c16345ba46c69bc2ef992a90d5967c5b
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 20:28:08 +0100

    Add: Services index for administration module

---
+++ src/modules/common/administration/services/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Administration Services
+ * Exports all services related to administration entities
+ * 
+ * @author CHOUABBIA Amine
+ * @created 01-03-2026
+ */
+
+export { stateService } from './StateService';
+export { localityService } from './LocalityService';
