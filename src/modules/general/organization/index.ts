d730e48670ec4ba27f7a0955cf79738f2e4c7d7a
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 21:01:02 +0100

    fix: add pages and components exports to organization module barrel

--- src/modules/general/organization/index.ts
+++ src/modules/general/organization/index.ts
@@ -1,7 +1,13 @@
 /**
  * General > Organization barrel.
+ * Central export for all organization module components
+ * 
+ * @author CHOUABBIA Amine
+ * @updated 01-03-2026
  */
 
 export * from './dto';
 export * from './services';
 export * from './utils';
+export * from './pages';
+export * from './components';
