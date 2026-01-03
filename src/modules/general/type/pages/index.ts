60efe899b112f78d1d82c53a798c896587d711cf
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 21:45:51 +0100

    feat: export StructureType pages from index

--- src/modules/general/type/pages/index.ts
+++ src/modules/general/type/pages/index.ts
@@ -1,5 +1,10 @@
 /**
- * Administration Pages - Central Export
+ * Structure Type Pages
+ * Export all structure type pages
+ * 
  * @author CHOUABBIA Amine
+ * @created 01-03-2026
  */
 
+export { default as StructureTypeList } from './StructureTypeList';
+export { default as StructureTypeEdit } from './StructureTypeEdit';
