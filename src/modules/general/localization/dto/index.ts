2b91652bbdc32a349f3aa144fa5f72d7670bccf8
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-03 20:53:22 +0100

    feat: create localization DTO barrel export

--- src/modules/general/localization/dto/index.ts
+++ src/modules/general/localization/dto/index.ts
@@ -1,8 +1,13 @@
 /**
- * Administration DTOs - Central Export
- * @author CHOUABBIA Amine
+ * Localization DTOs - Central Export
+ * Aligned with Backend DTOs from HyFloAPI
+ * 
+ * @author MEDJERAB Abir (Backend), CHOUABBIA Amine (Frontend)
+ * @updated 01-03-2026
  */
 
 export type { CountryDTO } from './CountryDTO';
 export type { StateDTO } from './StateDTO';
 export type { LocalityDTO } from './LocalityDTO';
+export type { LocationDTO } from './LocationDTO';
+export type { ZoneDTO } from './ZoneDTO';
