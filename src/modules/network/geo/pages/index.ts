3788f8d9d0212bd944c131e9f78570f8bfe084f3
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-06 14:58:52 +0100

    Add PipelineMapPage to pages exports

--- src/modules/network/geo/pages/index.ts
+++ src/modules/network/geo/pages/index.ts
@@ -4,7 +4,9 @@
  * 
  * @author CHOUABBIA Amine
  * @created 12-24-2025
+ * @updated 01-06-2026 - Added PipelineMapPage
  */
 
 export { NetworkMapPage } from './NetworkMapPage';
 export { GeoDebugPage } from './GeoDebugPage';
+export { PipelineMapPage } from './PipelineMapPage';
