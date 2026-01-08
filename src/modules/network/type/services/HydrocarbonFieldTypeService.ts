0fe9f058c65a14a3954bd3625f498c5abf3172ae
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-08 12:49:46 +0100

    feat: add getAllNoPagination method to HydrocarbonFieldTypeService

--- src/modules/network/type/services/HydrocarbonFieldTypeService.ts
+++ src/modules/network/type/services/HydrocarbonFieldTypeService.ts
@@ -8,7 +8,7 @@
  * 
  * @author MEDJERAB Abir (Backend), CHOUABBIA Amine (Frontend)
  * @created 06-26-2025
- * @updated 01-02-2026
+ * @updated 01-08-2026 - Added getAllNoPagination method
  */
 
 import axiosInstance from '@/shared/config/axios';
@@ -29,6 +29,14 @@ export class HydrocarbonFieldTypeService {
         sort: pageable.sort,
       },
     });
+    return response.data;
+  }
+
+  /**
+   * Get all hydrocarbon field types without pagination
+   */
+  static async getAllNoPagination(): Promise<HydrocarbonFieldTypeDTO[]> {
+    const response = await axiosInstance.get<HydrocarbonFieldTypeDTO[]>(`${BASE_URL}/all`);
     return response.data;
   }
 
