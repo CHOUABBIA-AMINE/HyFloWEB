d8233b7cec493869aba048b27260b600dd402870
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-15 23:20:13 +0100

    Update core DTO index: remove HydrocarbonFieldDTO, add ProcessingPlantDTO and ProductionFieldDTO

--- src/modules/network/core/dto/index.ts
+++ src/modules/network/core/dto/index.ts
@@ -1,9 +1,18 @@
+/**
+ * Network Core DTOs - Barrel Export
+ * 
+ * Core infrastructure DTOs for the Network module.
+ * 
+ * @updated 01-15-2026 - Removed HydrocarbonFieldDTO, added ProcessingPlantDTO and ProductionFieldDTO
+ */
+
+export { EquipmentDTO } from './EquipmentDTO';
 export { FacilityDTO } from './FacilityDTO';
-export { HydrocarbonFieldDTO } from './HydrocarbonFieldDTO';
+export { InfrastructureDTO } from './InfrastructureDTO';
 export { PipelineDTO } from './PipelineDTO';
+export { PipelineSegmentDTO } from './PipelineSegmentDTO';
 export { PipelineSystemDTO } from './PipelineSystemDTO';
+export { ProcessingPlantDTO } from './ProcessingPlantDTO';
+export { ProductionFieldDTO } from './ProductionFieldDTO';
 export { StationDTO } from './StationDTO';
 export { TerminalDTO } from './TerminalDTO';
-export { InfrastructureDTO } from './InfrastructureDTO';
-export { EquipmentDTO } from './EquipmentDTO';
-export { PipelineSegmentDTO } from './PipelineSegmentDTO';
