3ee38b14790f1159d8da34dfd71fb5c450969c6f
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-15 22:27:36 +0100

    Update pages index: add new pages, remove orphaned HydrocarbonField exports

--- src/modules/network/core/pages/index.ts
+++ src/modules/network/core/pages/index.ts
@@ -1,10 +1,31 @@
+/**
+ * Network Core Pages - Barrel Export
+ * 
+ * Updated: 01-15-2026
+ * - Added ProcessingPlant and ProductionField pages
+ * - Removed orphaned HydrocarbonField pages
+ */
+
+// Station pages
 export { default as StationList } from './StationList';
 export { default as StationEdit } from './StationEdit';
+
+// Terminal pages
 export { default as TerminalList } from './TerminalList';
 export { default as TerminalEdit } from './TerminalEdit';
-export { default as HydrocarbonFieldList } from './HydrocarbonFieldList';
-export { default as HydrocarbonFieldEdit } from './HydrocarbonFieldEdit';
+
+// Pipeline pages
 export { default as PipelineList } from './PipelineList';
 export { default as PipelineEdit } from './PipelineEdit';
+
+// Pipeline System pages
 export { default as PipelineSystemList } from './PipelineSystemList';
 export { default as PipelineSystemEdit } from './PipelineSystemEdit';
+
+// Processing Plant pages
+export { default as ProcessingPlantList } from './ProcessingPlantList';
+export { default as ProcessingPlantEdit } from './ProcessingPlantEdit';
+
+// Production Field pages
+export { default as ProductionFieldList } from './ProductionFieldList';
+export { default as ProductionFieldEdit } from './ProductionFieldEdit';
