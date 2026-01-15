018e561acf193a0f0c473b71d0c4fba2fa775da9
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-16 00:33:52 +0100

    refactor(geo): update index to export ProductionFieldMarkers

--- src/modules/network/geo/components/index.ts
+++ src/modules/network/geo/components/index.ts
@@ -1,9 +1,11 @@
 /**
  * Geo Components Exports
  * 
+ * Updated: 01-16-2026 - Renamed HydrocarbonFieldMarkers to ProductionFieldMarkers
+ * 
  * @author CHOUABBIA Amine
  * @created 12-24-2025
- * @updated 01-06-2026
+ * @updated 01-16-2026
  */
 
 export { MapView } from './MapView';
@@ -11,7 +13,9 @@ export { MapControls } from './MapControls';
 export { MapLegend } from './MapLegend';
 export { StationMarkers } from './StationMarkers';
 export { TerminalMarkers } from './TerminalMarkers';
-export { HydrocarbonFieldMarkers } from './HydrocarbonFieldMarkers';
+export { ProductionFieldMarkers } from './ProductionFieldMarkers';
+// Deprecated alias for backward compatibility
+export { ProductionFieldMarkers as HydrocarbonFieldMarkers } from './ProductionFieldMarkers';
 export { PipelinePolylines } from './PipelinePolylines';
 export { MarkerPopup } from './MarkerPopup';
 export { OfflineTileLayer } from './OfflineTileLayer';
