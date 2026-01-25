410e08a70c402a660eef49a742d99cb52fdcbbf1
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-25 15:47:24 +0100

    feat: Add index file for flow core pages export

---
+++ src/modules/flow/core/pages/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Flow Core Pages Export
+ * 
+ * @author CHOUABBIA Amine
+ * @created 01-25-2026
+ */
+
+export { ReadingList } from './ReadingList';
+export { ReadingEdit } from './ReadingEdit';
