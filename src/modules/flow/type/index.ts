63d965550dd745bf664dc8d94e4ec130811109bc
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-25 14:02:32 +0100

    feat: Add flow/type module index for centralized exports

---
+++ src/modules/flow/type/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Flow Type Module - Centralized Exports
+ * 
+ * Export all DTOs and services from flow/type module
+ * 
+ * @author CHOUABBIA Amine
+ * @created 01-25-2026
+ */
+
+// DTOs
+export * from './dto/OperationTypeDTO';
+export * from './dto/EventTypeDTO';
+
+// Services
+export * from './services';
