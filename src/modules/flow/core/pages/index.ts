09ced0d8be0ba9af5ecc1cbaf8dc1879e20278b0
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-27 11:15:32 +0100

    Export PendingReadingsList component

--- src/modules/flow/core/pages/index.ts
+++ src/modules/flow/core/pages/index.ts
@@ -3,7 +3,9 @@
  * 
  * @author CHOUABBIA Amine
  * @created 01-25-2026
+ * @updated 01-27-2026 - Added PendingReadingsList
  */
 
 export { ReadingList } from './ReadingList';
 export { ReadingEdit } from './ReadingEdit';
+export { PendingReadingsList } from './PendingReadingsList';
