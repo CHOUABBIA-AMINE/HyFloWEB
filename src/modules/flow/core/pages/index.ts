444e95e78e277b0be0114c8edbb6e0fc25d37fcd
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-28 14:58:06 +0100

    feat: Export ThresholdList and ThresholdEdit components

--- src/modules/flow/core/pages/index.ts
+++ src/modules/flow/core/pages/index.ts
@@ -3,9 +3,11 @@
  * 
  * @author CHOUABBIA Amine
  * @created 01-25-2026
- * @updated 01-27-2026 - Added PendingReadingsList
+ * @updated 01-28-2026 - Added ThresholdList and ThresholdEdit
  */
 
 export { ReadingList } from './ReadingList';
 export { ReadingEdit } from './ReadingEdit';
 export { PendingReadingsList } from './PendingReadingsList';
+export { ThresholdList } from './ThresholdList';
+export { ThresholdEdit } from './ThresholdEdit';
