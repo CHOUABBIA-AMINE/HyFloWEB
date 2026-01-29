4e69d307c92058dd6c7db6510678576de80eae44
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-29 11:34:57 +0100

    feat: Export ForecastList and ForecastEdit pages

--- src/modules/flow/core/pages/index.ts
+++ src/modules/flow/core/pages/index.ts
@@ -4,6 +4,7 @@
  * @author CHOUABBIA Amine
  * @created 01-25-2026
  * @updated 01-28-2026 - Added ThresholdList and ThresholdEdit
+ * @updated 01-29-2026 - Added ForecastList and ForecastEdit
  */
 
 export { ReadingList } from './ReadingList';
@@ -11,3 +12,5 @@ export { ReadingEdit } from './ReadingEdit';
 export { PendingReadingsList } from './PendingReadingsList';
 export { ThresholdList } from './ThresholdList';
 export { ThresholdEdit } from './ThresholdEdit';
+export { ForecastList } from './ForecastList';
+export { ForecastEdit } from './ForecastEdit';
