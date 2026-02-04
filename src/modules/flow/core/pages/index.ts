10ef63786ec32be5d39b8d903aa10295bff2a2f8
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-04 13:09:18 +0100

    feat: export SlotMonitoring page

--- src/modules/flow/core/pages/index.ts
+++ src/modules/flow/core/pages/index.ts
@@ -3,11 +3,13 @@
  * 
  * @author CHOUABBIA Amine
  * @created 01-25-2026
+ * @updated 02-04-2026 - Added SlotMonitoring
  * @updated 01-31-2026 - Added OperationValidation
  * @updated 01-28-2026 - Added ThresholdList and ThresholdEdit
  * @updated 01-29-2026 - Added ForecastList, ForecastEdit, OperationList, OperationEdit
  */
 
+export { default as SlotMonitoring } from './SlotMonitoring';
 export { ReadingList } from './ReadingList';
 export { ReadingEdit } from './ReadingEdit';
 export { PendingReadingsList } from './PendingReadingsList';
