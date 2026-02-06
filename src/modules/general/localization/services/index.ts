b65aeca9bc79c960c9b51b92ac3883caec76634b
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-06 19:11:27 +0100

    feat: export CoordinateService from localization services

--- src/modules/general/localization/services/index.ts
+++ src/modules/general/localization/services/index.ts
@@ -2,6 +2,7 @@
  * Localization Services - Central Export
  * 
  * All services for the localization module.
+ * Updated: 02-06-2026 - Added CoordinateService
  * Updated: 01-15-2026 - Added DistrictService
  * 
  * @author CHOUABBIA Amine
@@ -13,3 +14,4 @@ export { LocalityService } from './LocalityService';
 export { DistrictService } from './DistrictService';
 export { ZoneService } from './ZoneService';
 export { LocationService } from './LocationService';
+export { CoordinateService } from './CoordinateService';
