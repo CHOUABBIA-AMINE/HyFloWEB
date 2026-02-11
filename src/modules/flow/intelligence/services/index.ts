4a3250b75437c89a1e6999cd729dc851e4a11e3a
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-11 11:55:29 +0100

    feat(intelligence): create services barrel export
    - Export FlowMonitoringService
    - Export TrendGroupBy type
    - Enable clean imports from intelligence module
    
    Created: 2026-02-11

---
+++ src/modules/flow/intelligence/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Flow Intelligence Services - Barrel Export
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2026-02-11
+ */
+
+export { FlowMonitoringService } from './FlowMonitoringService';
+export type { TrendGroupBy } from './FlowMonitoringService';
