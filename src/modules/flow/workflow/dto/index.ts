87fe370e1ad219d2139e4ecc05db9a578eaba1bf
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2026-02-12 23:16:32 +0100

    update Flow DTO

---
+++ src/modules/flow/workflow/dto/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Workflow Services - Flow Workflow Module
+ * 
+ * Barrel export for workflow services.
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2026-02-11
+ * @package flow/workflow/dto
+ */
+
+export * from './ReadingSubmitRequestDTO';
+export * from './ReadingValidationRequestDTO';
