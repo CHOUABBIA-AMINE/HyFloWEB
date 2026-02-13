d52b1ed5975cb4d82e228aacc681e5cd44714671
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-13 18:03:27 +0100

    Fix: Create missing services/index.ts in flow/workflow
    - Export ReadingWorkflowService for module consumption
    - Resolves TS2307 error in flow/workflow/index.ts
    - Follows standard module export pattern
    
    Fixed: 02-13-2026

---
+++ src/modules/flow/workflow/services/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Flow Workflow Services
+ * 
+ * Exports all workflow-related services for flow reading state management.
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2026-02-13
+ * @package flow/workflow/services
+ */
+
+export { ReadingWorkflowService } from './ReadingWorkflowService';
