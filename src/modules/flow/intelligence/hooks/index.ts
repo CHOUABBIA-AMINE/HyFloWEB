3efb55e93a6e0de7522ce467f64b19de513cdbe5
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-14 18:05:26 +0100

    feat: Export intelligence hooks
    Create barrel export for intelligence hooks.
    Currently exports usePipelineDashboard.

---
+++ src/modules/flow/intelligence/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Flow Intelligence Hooks
+ * 
+ * @author CHOUABBIA Amine
+ * @created 02-14-2026
+ */
+
+export { usePipelineDashboard } from './usePipelineDashboard';
+export type { UsePipelineDashboardReturn, UsePipelineDashboardOptions } from './usePipelineDashboard';
