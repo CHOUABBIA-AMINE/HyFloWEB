105eb0a0e6c5dd3879a872155c6fe27923476baa
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-02-14 18:04:54 +0100

    feat: Export PipelineIntelligenceService
    Add PipelineIntelligenceService to barrel exports.
    Enable convenient imports throughout the application.

--- src/modules/flow/intelligence/services/index.ts
+++ src/modules/flow/intelligence/services/index.ts
@@ -3,7 +3,10 @@
  * 
  * @author CHOUABBIA Amine
  * @created 2026-02-11
+ * @updated 2026-02-14
  */
 
 export { FlowMonitoringService } from './FlowMonitoringService';
 export type { TrendGroupBy } from './FlowMonitoringService';
+
+export { PipelineIntelligenceService } from './PipelineIntelligenceService';
